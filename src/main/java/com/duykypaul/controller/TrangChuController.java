cb972fdc70b158bab8f4af5b0b6588642b39c103
duykypaul <duykypaul@users.noreply.github.com>
2019-08-19 14:31:17 +0700

    tạo database

--- src/main/java/com/duykypaul/controller/TrangChuController.java
+++ src/main/java/com/duykypaul/controller/TrangChuController.java
@@ -1,16 +1,12 @@
 package com.duykypaul.controller;
 
-import java.util.List;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.duykypaul.entity.NhanVien;
 
@@ -26,13 +22,10 @@ public class TrangChuController {
 	public String Default() {
 
 		Session session = sessionFactory.getCurrentSession();
+		/*
+		 * NhanVien duyky = session.get(NhanVien.class, 34); session.delete(duyky);
+		 */
 		
-		NhanVien suka = (NhanVien) session.createQuery("from NhanVien WHERE idNhanVien = 4").uniqueResult();
-		suka.setTuoi(25);
-		session.update(suka);
-		suka = session.get(NhanVien.class, 4);
-		suka.setTenNhanVien("suka beau");
-		session.update(suka);
 		return "trangchu";
 	}
 	
